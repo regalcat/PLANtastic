9ce7f3ce174916f03d670141cac02e43b744556d
semaslo <semaslo@iastate.edu>
2015-02-15 19:46:46 -0600

    Able to insert something using a model!

--- susan2/tools/item_share/models.py
+++ susan2/tools/item_share/models.py
@@ -3,7 +3,9 @@ from base.models import EventModel
 from base.models import UserModel
 
 class ItemModel(models.Model):
-	eventid = models.ForeignKey(EventModel)
+# TODO - Get EventModel from Megan
+#	eventid = models.ForeignKey(EventModel)
+	eventid = models.IntegerField()
 	itemid = models.IntegerField(primary_key=True)
 	item_name = models.CharField(max_length=50)
 	amount_needed = models.IntegerField()
--- susan2/tools/item_share/urls.py
+++ susan2/tools/item_share/urls.py
@@ -3,4 +3,5 @@ from tools.item_share import views
 
 urlpatterns = patterns('', 
 	url(r'^$', views.index, name='index'),
+	url(r'^form/$', views.form, name='form'),
 )
