d1b1d27d2452610087beacb5d5ba8264905d9a04
semaslo <semaslo@iastate.edu>
2015-02-16 01:12:32 -0600

    AddItemForm

--- susan2/tools/item_share/urls.py
+++ susan2/tools/item_share/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
 from tools.item_share import views
+from tools.item_share.AddItemView import AddItemView
 
 urlpatterns = patterns('', 
 	url(r'^$', views.index, name='index'),
 	url(r'^form/$', views.form, name='form'),
+	url(r'^add_item/$', AddItemView.as_view(), name='add-item')
 )
