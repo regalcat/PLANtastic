bf2062501a46be2be5b4c076b0721f36c9974225
erika <erikah@iastate.edu>
2015-02-19 08:28:06 -0600

    Updated views and started on a form for register a new user.

--- erika/register/urls.py
+++ erika/register/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, url
 from register import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.index, name = 'index'),
 	url(r'(?P<userID>\d+)/$', views.profile, name = 'profile'),
+	url(r'^register/$', views.register, name = 'register'),
 ) 
 
  
