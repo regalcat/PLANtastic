d16f3814c4d90faf766311619c865fa2b1fad8d2
Steff Bisinger <srbising@iastate.edu>
2015-02-19 10:40:49 -0600

    Starting work on forms.py

--- steff-test/base/invite/views.py
+++ steff-test/base/invite/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from base.forms import InviteForm
+from django.views.generic.edit import FormView
+
 
 def test(request):
 	template = loader.get_template('index.html')
 	context = RequestContext(request)
 	return HttpResponse(template.render(context))
 
-
+#The class that handles inviting people via email and then displays an html page
+class InviteView(forms.Form):
+	temp_name = ''
+	form_class = InviteForm
+	success_url = '/thanks/'
+	
+	def valid_email(self, form):
+		
+		form.send_invite()
+		return super(InviteView, self).valid_email(form)
