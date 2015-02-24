6a003ac2d527e7fcdf5e1c555fec3e60aafe648a
Steff Bisinger <srbising@iastate.edu>
2015-02-24 10:47:30 -0600

    Working my way through class

--- steff-test/base/forms.py
+++ steff-test/base/forms.py
@@ -8,7 +8,21 @@ from django.core.mail import send_mail
 class InviteForm(forms.Form):
 	email = forms.EmailField(required=True)
 	
+	#A rather long function that sends the email. Takes in the InviteForm object that called it.
 	def send_email(self):
-		send_mail('Invitation to join a PLANtastic event!', 
-			, 'plantastic@gmail.com', [email], html_message=invitemsg.html)
-		to
+		gmail_user = 'plantastic@gmail.com'
+		gmail_passwd = '' #This seems like a horrible idea. I'll find a better way later.
+		smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+		smtpserver.ehlo()
+		smtpserver.starttls()
+		smtpserver.login(gmail_user, gmail_passwd)
+		header = 'To:' + self.email +'\n' + gmail_user + '\n' + 'Subject: Plantastic Test Invite \n'
+		print header
+		msg = header + '\n This is a test email \n\n'
+		smtpserver.sendmail(gmail_user, to, msg)
+		print 'Email sent!'
+		smtpserver.close()
+		#Uncomment to use the Django send_mail function. (Doesn't work right now)
+		#send_mail('Invitation to join a PLANtastic event!', 
+		#	, 'plantastic@gmail.com', [email], html_message=invitemsg.html)
+		
---
+++ steff-test/base/views.py
@@ -0,0 +1,12 @@
+from base.forms import InviteForm
+from django.views.generic.edit import FormView
+
+class InviteView(FormView):
+	template_name = 'invite.html'
+	form_class = InviteForm
+	success_url = '/invtie-sent/'
+	
+	#Called when a valid form is submitted
+	def form_valid(self, form):
+		form.send_email()
+		return super(InviteView, self).form_valid(form)
