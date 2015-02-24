f610ec9d37036705b90666457f586163303b7c63
Steff Bisinger <srbising@iastate.edu>
2015-02-24 10:05:54 -0600

    Progress from last night

--- steff-test/base/forms.py
+++ steff-test/base/forms.py
@@ -1,4 +1,14 @@
+
+#python libraries
+import smtplib
+#django libraries
 from django import forms
+from django.core.mail import send_mail
 
 class InviteForm(forms.Form):
-	#TODO
+	email = forms.EmailField(required=True)
+	
+	def send_email(self):
+		send_mail('Invitation to join a PLANtastic event!', 
+			, 'plantastic@gmail.com', [email], html_message=invitemsg.html)
+		to
