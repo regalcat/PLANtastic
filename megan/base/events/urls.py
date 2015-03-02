2d3db384f5eca8aa8a5685aa0bee5ecaa96b3057
Megan <mksoll@iastate.edu>
2015-03-01 23:44:18 -0600

    finished new.html

--- megan/base/events/urls.py
+++ megan/base/events/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from events import views
 
 urlpatterns = patterns('', 
-	url(r'^$', views.index, name='index'),
+	url(r'^new/$', views.newEvent, name='newEvent'),
+	#url(r'^invite/$', views.inviteFriends, name='inviteFriends'
 )
 
