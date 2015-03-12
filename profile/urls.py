6b33ece4c3930d6bd86d719e7ff68499f906c373
erika <erikah@iastate.edu>
2015-03-12 17:46:26 -0500

    Some more changes with the new profile directory

--- profile/urls.py
+++ profile/urls.py
@@ -3,7 +3,8 @@ from profile import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.profile, name='profile'),
-	url(r'^manageAccount/$', views.manageAccount, name='manageAccount')
+	url(r'^manageAccount/$', views.manageAccount, name='manageAccount'),
+	url(r'^checkInformation/$', views.checkInformation, name='checkInformation'),
 
 
 )
