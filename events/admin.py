0345a3508e3018e1d2fea4bea5aa5c62eb17333a
Megan <mksoll@iastate.edu>
2015-04-01 12:13:23 -0500

    x

--- events/admin.py
+++ events/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from models import EventModel, HikeEventModel, GenericTripModel, GenericGatheringModel, DinnerModel
+from models import EventModel, HikeEventModel, GenericTripModel, GenericGatheringModel, DinnerEventModel
 
 admin.site.register(EventModel)
 admin.site.register(HikeEventModel)
 admin.site.register(GenericTripModel)
 admin.site.register(GenericGatheringModel)
-admin.site.register(DinnerModel)
+admin.site.register(DinnerEventModel)
