95b971680991ddcfaa7eb572024fc61ec8e3fe64
semaslo <semaslo@iastate.edu>
2015-04-01 11:39:22 -0500

    Moved things to users directory

--- base/views.py
+++ base/views.py
@@ -24,49 +24,7 @@ def index(request):
 def home(request):
 	# TODO
 	return render(request, 'home.html', {'menu' : getMenuInfo(request), 'title' : "Home"})
-
-
-def logout(request):
-	authLogout(request)
-	return HttpResponseRedirect(reverse('base:index'))
-
-def login(request):
-	return render(request, 'login.html')
-
-def authView(request):
-	username = request.POST.get('username')
-	password = request.POST.get('password')
-	
-	user = authenticate(username=username, password=password)
-
-	if user is not None:
-		authLogin(request, user)
-		return HttpResponseRedirect(reverse('base:home'))
-
-	else:
-		return HttpResponseRedirect(reverse('base:invalidLogin'))
-
-def invalidLogin(request):
-	return render(request, 'invalidLogin.html')
-
-def loginRequired(request):
-	return render(request, 'loginRequired.html')
-
-def registerSuccess(request):
-	return render(request, 'registerSuccess.html')
-
-def register(request):
-	if request.method == "POST":
-		form = UserRegistrationForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return HttpResponseRedirect(reverse('base:registerSuccess'))
-
-	args = {}
-	args['form'] = UserRegistrationForm()
-	return render(request, 'register.html', args)	
 		
-
 def coverPic(request):
 	files = '{"pics": ['
 	path = 'base/static/cover_pics/'
@@ -78,6 +36,3 @@ def coverPic(request):
 	files = files[:-1]
 	files += "]}"
 	return HttpResponse(files)
-
-
-
