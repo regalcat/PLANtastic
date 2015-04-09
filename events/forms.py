bc2a7ec4f102812b6c3d721658c9c4336e94246f
erika <erikah@iastate.edu>
2015-04-09 18:36:40 -0500

    See a member list on the events.

--- events/forms.py
+++ events/forms.py
@@ -8,7 +8,7 @@ class EventForm(forms.ModelForm):
 	class Meta:
 		model = EventModel
 		fields = ('eventName', 'eventLocation', 'eventDateStart', 'eventDescription', 'eventType',)
-		eventType = forms.ChoiceField()
+		eventType = forms.ChoiceField(choices=EventModel.EVENT_TYPES)
 		eventName = forms.CharField()
 		eventDescription = forms.CharField()
 		eventLocation = forms.CharField()
