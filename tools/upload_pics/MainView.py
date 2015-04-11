ae1a9e684c0f69eef072c9abff218ae161ea3265
semaslo <semaslo@iastate.edu>
2015-04-10 22:30:56 -0500

    Working on upload pics tool

--- tools/upload_pics/MainView.py
+++ tools/upload_pics/MainView.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+import os
+
 from base.helpers import getMenuInfo
 from events.models import EventModel
+from settings import settings
 
 from .models import UploadedPicModel
 
@@ -13,3 +16,13 @@ class MainView(View):
 		pics = UploadedPicModel.objects.filter(event_id = eventid)
 		return render(request, self.template_name, \
 			{'menu' : getMenuInfo(request), 'title' : "Upload Pics Tool", 'pics' : pics})
+
+	def post(self, request, eventid):
+		if request.POST['selected'] == 'None':
+			return self.get(request, eventid)
+		if 'delete' in request.POST:
+			UploadedPicModel.objects.get(file=request.POST['selected']).delete()
+			os.remove(os.path.join(settings.BASE_DIR,request.POST['selected']))
+		if 'edit' in request.POST:
+			# TODO
+			print "EDIT"
