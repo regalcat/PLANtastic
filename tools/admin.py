329e5d9f8612eb2e6ae11d0282f94f0878717e9d
erika <erikah@iastate.edu>
2015-04-14 09:44:13 -0500

    admin stuff

--- tools/admin.py
+++ tools/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from tools.item_share.models import ItemModel, ItemSignupModel
+from tools.allergy_list.models import AllergyModel
+from tools.upload_pics.models import UploadedPicModel
 
 admin.site.register(ItemModel)
 admin.site.register(ItemSignupModel)
+admin.site.register(AllergyModel)
+admin.site.register(UploadedPicModel)
+
