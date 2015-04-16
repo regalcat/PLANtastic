08f2b8e02477646cceb366a05f13be7cedf0b4cb
erika <erikah@iastate.edu>
2015-04-16 13:57:40 -0500

    some error handeling

--- tools/weather/views.py
+++ tools/weather/views.py
@@ -22,8 +22,17 @@ def showWeather(request, eventid):
 	information = WeatherModel.objects.filter(event = event)
 	if information.count() == 1:
 		context['state'] = information[0].get_state_display
-		context['city'] = information[0].city
 		context['degreeType'] = information[0].degreeType
+
+		city = information[0].city.title()
+		new = ""
+		for i in range(len(city)):
+			if city[i] == " ":
+				new = new + "+"
+			else:
+				new = new + city[i]
+
+		context['city'] = new
 
 
 	return render(request, 'weather/showWeather.html', context)
