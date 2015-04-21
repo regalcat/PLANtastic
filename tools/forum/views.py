fb8e187045235946958bee6f8311900031fa01f7
Steff Bisinger <srbising@iastate.edu>
2015-04-21 00:07:56 -0500

    More progress on forum. Views almost done. Templates nearly done.

--- tools/forum/views.py
+++ tools/forum/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-
+from django.views.generic import View
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 
@@ -16,10 +16,13 @@ class ForumView(ToolView):
 	def getIdentifier():
 		return "forum"
 	
+	#returns the number of posts in the given thread
 	def numPosts(cur_thread):
-		num = PostModel.posts.filter(thread = cur_thread)
+		num = len(PostModel.posts.filter(thread = cur_thread))
 		return num
 
+	#Returns the template for the forum tile in the event page
+	@method_decorator
 	def get(self, request, eventid):
 		user = request.user
 		cur_event = EventModel.getEvent(eventid)
@@ -29,8 +32,35 @@ class ForumView(ToolView):
 		context = RequestContext(request, {'threads' : threads, 'event' : cur_event, 'cur_path' : request.get_full_path(), 'title' : forum.title, 'menu' : getMenuInfo(request) })
 		return HttpResponse(template.render(context))
 
+	#Adds the new thread and the initial post and returns the get view
+	@method_decorator
 	def post(self, request, eventid):
 		cur_event = getEvent(eventid)
-		#TODO - Add the new thread to the forum
+		ttitle = request.POST['ttitle']
+		tforum = ForumModel.forums.filter(event = cur_event)
+		tcreator = request.user
+		thread = ThreadModel(title=ttitle, creator=tcreator, forum=tforum)
+		thread.save()
+		pbody = request.POST['pbody']
+		post = PostModel(title=ttitle, creator=tcreator, thread=thread, body=pbody)
+		post.save()
 		return self.get(request, eventid)
 
+class ThreadView(View):
+	template = "forum/threadTemplate.html"
+	
+	#Gets a paginated list of the posts in the current thread
+	@method_decorator
+	def get(self, request, eventid, key):
+		cur_event = EventModel.getEvent(eventid)
+		posts = Post.posts.filter(thread=key).order_by("created")
+		title = ThreadModel.threads.get(pk=key).title
+		context = RequestContext(request, {'posts' : posts, 'thread' : ThreadModel.threads,get(pk=key), 'event' : cur_event, 'cur_path' : request.get_full_path(), 'title' : title, 'menu' : getMenuInfo(request) })
+		return HttpResponse(template.render(context))
+	
+	#Adds the post to the thread and returns the get method
+	@method_decorator
+	def post(self, request, eventid):
+		cur_event = EventModel.getEvent(eventid)
+		
+		return self.get(request, eventid)
