28d21ea4e7dceb6df2fed77fcacabc7c915a8764
erika <erikah@iastate.edu>
2015-04-27 13:27:59 -0500

    admin stuff

--- tools/admin.py
+++ tools/admin.py
@@ -3,7 +3,8 @@ from tools.item_share.models import ItemModel, ItemSignupModel
 from tools.allergy_list.models import AllergyModel
 from tools.upload_pics.models import UploadedPicModel
 from tools.weather.models import WeatherModel
-from tools.ride_share.models import Car,Person, Riders
+from tools.ride_share.models import Car, Person, Riders
+from tools.schedule.models import ScheduleModel
 
 admin.site.register(ItemModel)
 admin.site.register(ItemSignupModel)
@@ -13,4 +14,5 @@ admin.site.register(WeatherModel)
 admin.site.register(Car)
 admin.site.register(Person)
 admin.site.register(Riders)
+admin.site.register(ScheduleModel)
 
