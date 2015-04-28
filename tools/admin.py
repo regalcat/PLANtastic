48a52a252fff3cd329c529fcc65c90c4fba33c11
erika <erikah@iastate.edu>
2015-04-28 14:10:03 -0500

    admin stuff

--- tools/admin.py
+++ tools/admin.py
@@ -6,6 +6,8 @@ from tools.weather.models import WeatherModel
 from tools.ride_share.models import Car, Person, Riders
 from tools.schedule.models import ScheduleModel
 from tools.maps.models import Gmap
+from tools.forum.models import ForumModel, ThreadModel, PostModel
+from tools.money_share.models import MsSettingsModel, MsItemModel, MsSplitsModel, MsPaymentModel
 
 admin.site.register(ItemModel)
 admin.site.register(ItemSignupModel)
@@ -17,4 +19,10 @@ admin.site.register(Person)
 admin.site.register(Riders)
 admin.site.register(ScheduleModel)
 admin.site.register(Gmap)
-
+admin.site.register(ForumModel)
+admin.site.register(ThreadModel)
+admin.site.register(PostModel)
+admin.site.register(MsSettingsModel)
+admin.site.register(MsItemModel)
+admin.site.register(MsSplitsModel)
+admin.site.register(MsPaymentModel)
