6b9e4dd251cf2046cd319fd8edbec6171d7a45c5
Megan <mksoll@iastate.edu>
2015-04-29 10:20:27 -0500

    prettifying event home page

--- tools/ToolManager.py
+++ tools/ToolManager.py
@@ -23,26 +23,29 @@ class ToolManager:
 		context = {}
 		# Tools for Previous Events
 		if isPreviousEvent(event):
+			tools.append(MoneyShareTool)
+			context['money_share'] = MoneyShareTool.getContext(event)
 			tools.append(UploadPicsView)
 			context['upload_pics'] = UploadPicsView.getContext(event)
 		# Tools for Upcoming Events
 		if isUpcomingEvent(event):
 			tools.append(ItemShareView)
 			context['item_share'] = ItemShareView.getContext(event)
-			tools.append(WeatherView)
-			context['weather'] = WeatherView.getContext(event)
-			tools.append(AllergyListView)
-			context['allergy_list'] = AllergyListView.getContext(event)
-			tools.append(RideshareView)
-			context['ride_share'] = RideshareView.getContext(event)
 			tools.append(ScheduleView)
 			context['schedule'] = ScheduleView.getContext(event)
+			tools.append(MoneyShareTool)
+			context['money_share'] = MoneyShareTool.getContext(event)
+			tools.append(AllergyListView)
+			context['allergy_list'] = AllergyListView.getContext(event)
+			tools.append(WeatherView)
+			context['weather'] = WeatherView.getContext(event)
+			tools.append(RideshareView)
+			context['ride_share'] = RideshareView.getContext(event)
 
 		# Tools for both Upcoming and Previous Events
-		tools.append(MoneyShareTool)
-		context['money_share'] = MoneyShareTool.getContext(event)
-		tools.append(MapView)
-		context['maps'] = MapView.getContext(event)
+		
 		tools.append(ForumView)
 		context['forum'] = ForumView.getContext(event)
+		tools.append(MapView)
+		context['maps'] = MapView.getContext(event)
 		return {'tools' : tools, 'context' : context}
